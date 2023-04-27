1f11c2a0eb60e796dd7525bad267a89c2e86a2bc
dawkaka <yousiph77@gmail.com>
2023-04-27 15:12:51 +0000

    done

--- src/components/tank.tsx
+++ src/components/tank.tsx
@@ -5,7 +5,6 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 export default function Tank() {
     const canvasRef = useRef<HTMLCanvasElement>(null)
     useEffect(() => {
-
         const renderer = new THREE.WebGLRenderer({ canvas: canvasRef.current! })
         renderer.setClearColor(0xAAAAAA);
         renderer.shadowMap.enabled = true;
@@ -32,7 +31,8 @@ export default function Tank() {
             light.shadow.mapSize.width = 2048;
             light.shadow.mapSize.height = 2048;
 
-            const d = 50;
+
+            const d = 100;
             light.shadow.camera.left = -d;
             light.shadow.camera.right = d;
             light.shadow.camera.top = d;
@@ -55,68 +55,114 @@ export default function Tank() {
         groundMesh.receiveShadow = true;
         scene.add(groundMesh);
 
-        const h = 1
-        const w = 4
-        const l = 8
+        const carHeight = 1
+        const carWidth = carHeight * 4
+        const carLength = carWidth * 2
 
         const tank = new THREE.Object3D()
         scene.add(tank)
 
-        const carGeo = new THREE.BoxGeometry(w, h, l)
+        const carGeo = new THREE.BoxGeometry(carWidth, carHeight, carLength)
         const carMesh = new THREE.Mesh(carGeo, new THREE.MeshPhongMaterial({ color: 0x6688AA }))
-        carMesh.scale.set(2, 2, 2)
         carMesh.castShadow = true
-        carMesh.position.y = 2.3
+        carMesh.position.y = carHeight * 1.5
         tank.add(carMesh)
 
-        const sphereGeo = new THREE.CylinderGeometry(2, 2, 1, 6)
 
-        const wheel1 = new THREE.Mesh(sphereGeo, new THREE.MeshPhongMaterial({ color: 0x6688AA }))
-        wheel1.position.y = 1.7
-        wheel1.position.z = 6
-        wheel1.position.x = 4.5
-        wheel1.rotation.z = 1.6
-        wheel1.castShadow = true
-        const wheel2 = new THREE.Mesh(sphereGeo, new THREE.MeshPhongMaterial({ color: 0x6688AA }))
-        wheel2.position.y = 1.7
-        wheel2.position.x = 4.5
-        wheel2.rotation.z = 1.6
-        wheel2.castShadow = true
+        const wheelRad = carHeight
+        const wheelTickness = carHeight / 2
+        const sphereGeo = new THREE.CylinderGeometry(wheelRad, wheelRad, wheelTickness, 6)
 
-        const wheel3 = new THREE.Mesh(sphereGeo, new THREE.MeshPhongMaterial({ color: 0x6688AA }))
-        wheel3.position.y = 1.7
-        wheel3.position.z = -6
-        wheel3.position.x = 4.5
-        wheel3.rotation.z = 1.6
-        wheel3.castShadow = true
+        let wheelsPositions = [
+            { x: carWidth / 2 + wheelTickness / 2, z: carLength / 2 - wheelRad },
+            { x: carWidth / 2 + wheelTickness / 2, z: 0 },
+            { x: carWidth / 2 + wheelTickness / 2, z: -1 * (carLength / 2 - (wheelRad)) },
+            { x: -1 * (carWidth / 2 + wheelTickness / 2), z: carLength / 2 - wheelRad },
+            { x: -1 * (carWidth / 2 + wheelTickness / 2), z: 0 },
+            { x: -1 * (carWidth / 2 + wheelTickness / 2), z: -1 * (carLength / 2 - (wheelRad)) },
+        ]
 
-        const wheel4 = new THREE.Mesh(sphereGeo, new THREE.MeshPhongMaterial({ color: 0x6688AA }))
-        wheel4.position.y = 1.7
-        wheel4.position.z = -6
-        wheel4.position.x = -4.5
-        wheel4.rotation.z = 1.6
-        wheel4.castShadow = true
+        const wheels = wheelsPositions.map((p) => {
+            const wheel = new THREE.Mesh(sphereGeo, new THREE.MeshPhongMaterial({ color: 0x6688AA }))
+            wheel.rotation.z = 0.5 * Math.PI
+            wheel.position.y = wheelRad
+            wheel.position.x = p.x
+            wheel.position.z = p.z
+            wheel.castShadow = true
+            tank.add(wheel)
+            return wheel
+        })
 
-        const wheel5 = new THREE.Mesh(sphereGeo, new THREE.MeshPhongMaterial({ color: 0x6688AA }))
-        wheel5.position.y = 1.7
-        wheel5.position.x = -4.5
-        wheel5.rotation.z = 1.6
-        wheel5.castShadow = true
+        const combRad = carWidth / 2
+        const combMesh = new THREE.Mesh(new THREE.SphereGeometry(combRad, 12, 12, 0, Math.PI, 0, Math.PI), new THREE.MeshPhongMaterial({ color: 0x6688AA }))
+        combMesh.position.y = combRad
+        combMesh.rotation.x = -0.5 * Math.PI
+        tank.add(combMesh)
 
-        const wheel6 = new THREE.Mesh(sphereGeo, new THREE.MeshPhongMaterial({ color: 0x6688AA }))
-        wheel6.position.y = 1.7
-        wheel6.position.z = 6
-        wheel6.position.x = -4.5
-        wheel6.rotation.z = 1.6
-        wheel6.castShadow = true
-        const wheels = [wheel1, wheel2, wheel3, wheel4, wheel5, wheel6]
+        const pivot = new THREE.Object3D()
+        tank.add(pivot)
+        const turretHeight = carHeight * 0.5
+        const turretLength = carLength * 0.75
+        const turretY = carHeight * 1.5 + turretHeight
+        const turretZ = ((turretLength) / 2)
+        const turretRotation = 20
+        const turret = new THREE.Mesh(new THREE.BoxGeometry(turretHeight, turretHeight, turretLength), new THREE.MeshPhongMaterial({ color: 0x6688AA }))
+        turret.castShadow = true
+        pivot.add(turret)
 
-        tank.add(wheel1)
-        tank.add(wheel2)
-        tank.add(wheel3)
-        tank.add(wheel4)
-        tank.add(wheel5)
-        tank.add(wheel6)
+        pivot.position.y = turretY
+        turret.position.z = turretZ
+
+        const turretCamera = makeCamera();
+        turretCamera.position.y = .5
+        turret.add(turretCamera);
+
+
+        const targetOrbit = new THREE.Object3D()
+        const targetElevation = new THREE.Object3D();
+        const targetBob = new THREE.Object3D();
+
+        scene.add(targetOrbit);
+        targetOrbit.add(targetElevation);
+        targetElevation.position.z = turretZ + 20
+        targetElevation.position.y = (turretZ + 20) / Math.tan(turretRotation) + turretHeight
+        targetElevation.add(targetBob);
+
+        const target = new THREE.Mesh(new THREE.SphereGeometry(1, 12, 12, 0, 360, 0, 360), new THREE.MeshPhongMaterial({ color: 0xFFF000 }))
+        targetElevation.add(target)
+        target.castShadow = true
+
+        targetBob.add(target);
+        scene.add(targetOrbit)
+        targetOrbit.add(targetElevation)
+
+        const targetCamera = makeCamera();
+        const targetCameraPivot = new THREE.Object3D();
+        targetCamera.position.y = 1;
+        targetCamera.position.z = -2;
+        targetCamera.rotation.y = Math.PI;
+        targetBob.add(targetCameraPivot);
+        targetCameraPivot.add(targetCamera);
+
+        const curve = new THREE.SplineCurve([
+            new THREE.Vector2(-10, 0),
+            new THREE.Vector2(-5, 5),
+            new THREE.Vector2(0, 0),
+            new THREE.Vector2(5, -5),
+            new THREE.Vector2(10, 0),
+            new THREE.Vector2(5, 10),
+            new THREE.Vector2(-5, 10),
+            new THREE.Vector2(-10, -10),
+            new THREE.Vector2(-15, -8),
+            new THREE.Vector2(-10, 0),
+        ]);
+
+        const points = curve.getPoints(50)
+        const splineObject = new THREE.Line(new THREE.BufferGeometry().setFromPoints(points), new THREE.MeshBasicMaterial({ color: 0xFF0000 }))
+        splineObject.rotation.x = 0.5 * Math.PI
+        splineObject.position.y = 0.05;
+
+        scene.add(splineObject)
 
         function resizeRendererToDisplaySize(renderer: THREE.WebGLRenderer) {
             const canvas = renderer.domElement;
@@ -129,7 +175,16 @@ export default function Tank() {
             return needResize
         }
 
+        const targetPosition = new THREE.Vector3();
+        const tankPosition = new THREE.Vector2();
+        const tankTarget = new THREE.Vector2();
+        const cameras = [camera, turretCamera, targetCamera]
+        console.log(curve)
+
+
         function render(time: number) {
+
+            time *= 0.001
             if (resizeRendererToDisplaySize(renderer)) {
                 const canvas = renderer.domElement;
                 camera.aspect = canvas.clientWidth / canvas.clientHeight;
@@ -137,11 +192,34 @@ export default function Tank() {
             }
 
             wheels.forEach((wheel) => {
-                wheel.rotation.x = time * 0.003
+                wheel.rotation.x = time * 3
             })
 
+            targetBob.position.y = Math.sin(time * 2) * 4
+
+            targetOrbit.rotation.y = time * .27
+            target.rotation.y = time * 7
+            target.rotation.x = time * 13
+            target.rotation.z = time * 5
+
+            const tankTime = time * .05;
+            curve.getPointAt(tankTime % 1, tankPosition);
+            curve.getPointAt((tankTime + 0.01) % 1, tankTarget);
+            tank.position.set(tankPosition.x, 0, tankPosition.y);
+            tank.lookAt(tankTarget.x, 0, tankTarget.y);
+
+
+            tank.getWorldPosition(targetPosition);
+            targetCameraPivot.lookAt(targetPosition);
+
+            target.getWorldPosition(targetPosition);
+            pivot.lookAt(targetPosition);
+            turretCamera.lookAt(targetPosition)
+
+            const cam = cameras[time % cameras.length | 0]
             renderer.render(scene, camera)
             requestAnimationFrame(render)
+
         }
         requestAnimationFrame(render)
     }, [])
@@ -149,8 +227,8 @@ export default function Tank() {
     return (
         <canvas ref={canvasRef}
             style={{
-                height: "500px",
-                width: "1000px",
+                height: "100%",
+                width: "100%",
             }}
         >
         </canvas>
