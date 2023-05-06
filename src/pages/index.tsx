ea78095a61c6f5cf44b54bf9dd66d82229a64ce3
dawkaka <yousiph77@gmail.com>
2023-05-06 11:54:24 +0000

    hm

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,6 +23,7 @@ export default function Home() {
         <Primitives />
         <SceneGraph />
         <BuffferGeometry />
+        <Shpere />
       </main>
     </>
   )
@@ -127,9 +128,10 @@ function BuffferGeometry() {
     geometry.setAttribute(
       'position',
       new THREE.BufferAttribute(new Float32Array(positions), positionNumComponents));
-    geometry.setAttribute(
-      'normal',
-      new THREE.BufferAttribute(new Float32Array(normals), normalNumComponents));
+    // geometry.setAttribute(
+    //   'normal',
+    //   new THREE.BufferAttribute(new Float32Array(normals), normalNumComponents));
+
     geometry.setAttribute(
       'uv',
       new THREE.BufferAttribute(new Float32Array(uvs), uvNumComponents));
@@ -141,6 +143,8 @@ function BuffferGeometry() {
       16, 17, 18, 18, 17, 19,
       20, 21, 22, 22, 21, 23
     ])
+    geometry.computeVertexNormals()
+
 
 
     const loader = new THREE.TextureLoader();
@@ -181,15 +185,179 @@ function BuffferGeometry() {
         camera.updateProjectionMatrix();
       }
 
-      // cubes.forEach((cube, ndx) => {
-      //   const speed = 1 + ndx * .1;
-      //   const rot = time * speed;
-      //   cube.rotation.x = rot;
-      //   cube.rotation.y = rot;
-      // });
+
+      cubes.forEach((cube, ndx) => {
+        const speed = 1 + ndx * .1;
+        const rot = time * speed;
+        cube.rotation.x = rot;
+        cube.rotation.y = rot;
+      });
 
       renderer.render(scene, camera);
 
+      requestAnimationFrame(render);
+    }
+
+    requestAnimationFrame(render);
+  }, [])
+
+  return (
+    <canvas ref={ref} style={{ width: "600px", height: "300px" }}></canvas>
+  )
+}
+
+
+
+function Shpere() {
+  const ref = useRef<HTMLCanvasElement>(null)
+
+
+  useEffect(() => {
+    const renderer = new THREE.WebGLRenderer({ antialias: true, canvas: ref.current! });
+
+    const fov = 75;
+    const aspect = 2;  // the canvas default
+    const near = 0.1;
+    const far = 100;
+    const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    camera.position.z = 5;
+
+    new OrbitControls(camera, renderer.domElement)
+
+    const scene = new THREE.Scene();
+
+    function addLight(x: number, y: number, z: number) {
+      const color = 0xFFFFFF;
+      const intensity = 1;
+      const light = new THREE.DirectionalLight(color, intensity);
+      light.position.set(x, y, z);
+      scene.add(light);
+    }
+    addLight(-1, 2, 4);
+    addLight(2, -2, 3);
+
+
+    function makeSphere(segDown: number, segAround: number) {
+      let positions = new Float32Array(segDown * segAround * 6 * 3)
+      const numComponents = 3
+      let indices = []
+
+      const longHelper = new THREE.Object3D();
+      const latHelper = new THREE.Object3D();
+      const pointHelper = new THREE.Object3D();
+      longHelper.add(latHelper);
+      latHelper.add(pointHelper);
+      pointHelper.position.z = 1;
+      const temp = new THREE.Vector3();
+
+      function getPoint(lat: number, long: number) {
+        latHelper.rotation.x = lat;
+        longHelper.rotation.y = long;
+        longHelper.updateMatrixWorld(true);
+        return pointHelper.getWorldPosition(temp).toArray();
+      }
+
+      let posNdx = 0
+      let ndx = 0
+      for (let down = 0; down < segDown; down++) {
+        const v0 = down / segDown;
+        const v1 = (down + 1) / segDown;
+        const lat0 = (v0 - 0.5) * Math.PI;
+        const lat1 = (v1 - 0.5) * Math.PI;
+
+        for (let across = 0; across < segAround; ++across) {
+          const u0 = across / segAround;
+          const u1 = (across + 1) / segAround;
+          const long0 = u0 * Math.PI * 2;
+          const long1 = u1 * Math.PI * 2;
+
+          positions.set(getPoint(lat0, long0), posNdx); posNdx += numComponents;
+          positions.set(getPoint(lat1, long0), posNdx); posNdx += numComponents;
+          positions.set(getPoint(lat0, long1), posNdx); posNdx += numComponents;
+          positions.set(getPoint(lat1, long1), posNdx); posNdx += numComponents;
+
+          indices.push(
+            ndx, ndx + 1, ndx + 2,
+            ndx + 2, ndx + 1, ndx + 3,
+          );
+          ndx += 4;
+        }
+      }
+      return { positions, indices }
+    }
+
+
+    const segmentsAround = 24;
+    const segmentsDown = 16;
+    const { positions, indices } = makeSphere(segmentsAround, segmentsDown);
+    const normals = positions.slice()
+
+    const geometry = new THREE.BufferGeometry();
+    const positionNumComponents = 3;
+    const normalNumComponents = 3;
+
+    const positionAttribute = new THREE.BufferAttribute(positions, positionNumComponents);
+    positionAttribute.setUsage(THREE.DynamicDrawUsage);
+    geometry.setAttribute(
+      'position',
+      positionAttribute);
+    geometry.setAttribute(
+      'normal',
+      new THREE.BufferAttribute(normals, normalNumComponents));
+    geometry.setIndex(indices);
+
+
+    function makeInstance(geometry: any, color: any, x: any) {
+      const material = new THREE.MeshPhongMaterial({
+        color,
+        side: THREE.DoubleSide,
+        shininess: 100,
+      });
+
+      const cube = new THREE.Mesh(geometry, material);
+      scene.add(cube);
+
+      cube.position.x = x;
+      return cube;
+    }
+
+    const cubes = [
+      makeInstance(geometry, 0xFF0000, 0),
+    ];
+
+    function resizeRendererToDisplaySize(renderer: THREE.Renderer) {
+      const canvas = renderer.domElement;
+      const width = canvas.clientWidth;
+      const height = canvas.clientHeight;
+      const needResize = canvas.width !== width || canvas.height !== height;
+      if (needResize) {
+        renderer.setSize(width, height, false);
+      }
+      return needResize;
+    }
+    const temp = new THREE.Vector3();
+
+    function render(time: number) {
+      time *= 0.001;
+
+      if (resizeRendererToDisplaySize(renderer)) {
+        const canvas = renderer.domElement;
+        camera.aspect = canvas.clientWidth / canvas.clientHeight;
+        camera.updateProjectionMatrix();
+      }
+
+      for (let i = 0; i < positions.length; i += 3) {
+        const quad = (i / 12 | 0);
+        const ringId = quad / segmentsAround | 0;
+        const ringQuadId = quad % segmentsAround;
+        const ringU = ringQuadId / segmentsAround;
+        const angle = ringU * Math.PI * 2;
+        temp.fromArray(normals, i);
+        temp.multiplyScalar(THREE.MathUtils.lerp(1, 1.4, Math.sin(time + ringId + angle) * .5 + .5));
+        temp.toArray(positions, i);
+      }
+      positionAttribute.needsUpdate = true;
+      renderer.render(scene, camera);
       requestAnimationFrame(render);
     }
 
