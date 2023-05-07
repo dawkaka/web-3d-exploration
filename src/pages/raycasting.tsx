2bbeaffe700712b62cec7cedc851e8db0c286463
dawkaka <yousiph77@gmail.com>
2023-05-07 20:43:27 +0000

    rems thatshit

--- src/pages/raycasting.tsx
+++ src/pages/raycasting.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from "react"
 import * as THREE from "three"
-import { addjust } from "./newton-craddle"
+import { addjust } from "./newton-cradle"
 
 export default function Page() {
     const canvasRef = useRef<HTMLCanvasElement>(null)
